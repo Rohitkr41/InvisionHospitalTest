7d0ef6ba1ac6c180feef4cbbcc4630dfff19341d
rohit kumar <138009969+Rohitkr41@users.noreply.github.com>
2026-06-15 14:07:58 +0530

    Update HEyeExaminationActionTest.java

--- src/test/java/tests/eyeExaminationSearch/HEyeExaminationActionTest.java
+++ src/test/java/tests/eyeExaminationSearch/HEyeExaminationActionTest.java
@@ -34,7 +34,7 @@ public class HEyeExaminationActionTest extends BaseTest{
     public void testAdvanceSearchAndClickPlus() throws InterruptedException {
 
         // SEARCH USING DATE FILTER
-        page.searchByDate("11-06-2026", "30-06-2026");
+        page.searchByDate("10-06-2026", "30-06-2026");
 
         // CLICK FIRST ROW PLUS ICON
         page.clickFirstRowPlusIcon();
